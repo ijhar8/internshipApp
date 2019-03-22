02ba021ef7f3b3a03bc03e1f3bc88a61d954dc5e
ijhar8 <ijharansari8@gmail.com>
2019-03-22 23:01:55 +0530

    file system

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { MainContainer } from './src/components/MainContainer';
-export default class App extends Component {
+
+export  class MainContainer extends Component {
+    
+
   render() {
     return (
-      <MainContainer />
+      <View >
+        <Text >im main</Text>
+      </View>
     );
   }
 }
